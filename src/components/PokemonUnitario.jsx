cc4b4dcf4742034b9b26f96caaae17fd9401dbd0
ErikSanzana <ersanzana@gmail.com>
2023-10-15 19:53:26 -0300

    sin css en los jsx y mostrando tipo

--- src/components/PokemonUnitario.jsx
+++ src/components/PokemonUnitario.jsx
@@ -3,7 +3,7 @@ import { Card } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 const PokemonUnitario = ({ pokemon }) => {
-  const { name, stats, src, types } = pokemon || {};
+  const { name, stats, src, type } = pokemon || {};
 
   const navigate = useNavigate();
   const handleClick = () => {
@@ -16,45 +16,21 @@ const PokemonUnitario = ({ pokemon }) => {
     </li>
   ));
 
-  const cardStyles = {
-    width: "37rem",
-    margin: "5px auto",
-    padding: "20px",
-    borderRadius: "15px",
-    backgroundColor: "white", // Fondo blanco
-    boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)", // Sutil sombra
-  };
-
-  const titleStyles = {
-    fontSize: "2rem",
-    fontWeight: "bold",
-  };
-
-  const cardTextStyles = {
-    backgroundColor: "red",
-    color: "white",
-    padding: "10px",
-    borderRadius: "5px",
-    fontWeight: "bold",
-    textTransform: "capitalize",
-    marginTop: "15px",
-  };
-
   return (
     <section className="container text-center">
-      <Card style={cardStyles}>
+      <Card>
         <Card.Img height="300" src={src} alt={name} />
         <Card.Body>
-          <Card.Title as="h1" style={titleStyles}>
-            {name}
-          </Card.Title>
-          <span className="list-unstyled text-start">
-            <Card.Text style={titleStyles}>{statList}</Card.Text>
-          </span>
-          <Card.Text style={cardTextStyles}>{types}</Card.Text>
+          <Card.Title as="h1">{name}</Card.Title>
+          <ul className="list-unstyled text-start">
+            {statList}
+          </ul>
+          <Card.Text className="bg-danger text-white p-2 rounded fw-bold text-capitalize mt-3">
+            {type}
+          </Card.Text>
         </Card.Body>
       </Card>
-      <button onClick={handleClick} className="btn btn-info btn-lg btn-block mt-1">
+      <button onClick={handleClick} className="btn btn-info btn-lg btn-block mt-3">
         Back
       </button>
     </section>
